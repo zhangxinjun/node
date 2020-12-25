3c25e2dfbe30766eeac9affa8349812f2e0fba7a
PS2020GXFJQFOU\Admin <759561217@qq.com>
2020-12-25 17:07:40 +0800

    使用axios下载图片

--- wenjianliu/index.js
+++ wenjianliu/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 const request = require("request")
 const https = require("https")
-
+const axios = require("axios")
 // function a () {
 //   request("https://www.valentino.com/variants/images/7789028784813600/R/w400.jpg").pipe(fs.createWriteStream("a.jpg").on("close",err => {
 //     console.log(err)
@@ -9,19 +9,32 @@ const https = require("https")
 // }
 // a()
 
-const options ={
-  "hostname":"www.valentino.com",
-  "path":"/variants/images/7789028784813600/R/w400.jpg",
-  "method":"GET",
-  "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"
-}
-https.get(options,res =>{
-  console.log(res.headers)
-  res.on("data",d => {
-    console.log(d)
+// const options ={
+//   "hostname":"www.valentino.com",
+//   "path":"/variants/images/7789028784813600/R/w400.jpg",
+//   "method":"GET",
+//   "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"
+// }
+// https.get("https://www.valentino.com/variants/images/2204324140530012/F/w400.jpg",res =>{
+//   console.log(res.headers)
+//   res.on("data",d => {
+//     console.log(d)
    
-  })
-  res.on("error",e => {
-    console.log(e)
-  })
+//   })
+//   res.on("error",e => {
+//     console.log(e)
+//   })
+// })
+
+axios.get("https://www.valentino.com/variants/images/2204324140530012/F/w400.jpg",{
+  responseType: 'arraybuffer'
+}).then(res => {
+  console.log(res)
+  // res.pipe(fs.createWriteStream("a.jpg").on("close",err => {
+  //   console.log(err)
+  // }))
+  fs.writeFile('out.png', res.data, function (err) {
+    if (err) throw err;
+    console.log('保存成功');
+ });
 })
